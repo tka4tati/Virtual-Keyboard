791ad59ed578549f00cef9c67aaf1f31e4196f82
Tatiana <tka4tati@gmail.com>
2023-04-30 20:16:31 +0300

    feat: create buttons in each row and add symbols to each button

--- script.js
+++ script.js
@@ -107,25 +107,150 @@ const keys1 = [
   const row1 = document.createElement("div");
   row1.classList.add("keyboard__row");
   section.appendChild(row1);
+
+  // кнопки первого ряда
+  for (let i = 0; i < keys1.length; i++) {
+    const key = document.createElement("div");
+    key.classList.add("keyboard__btn");
+    row1.appendChild(key);
+    for (let j = 0; j < keys1[i].length; j++) {
+        if (keys1[i].length === 2) {
+            const additionalSymbol = document.createElement("span");
+            additionalSymbol.classList.add("additional-symbol");
+            additionalSymbol.textContent = keys1[i][0];
+            key.appendChild(additionalSymbol);
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys1[i][j+1];
+            key.appendChild(mainSymbol);
+        }
+        else {
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys1[i][0];
+            key.appendChild(mainSymbol);
+        }
+    }
+ }
   
   //второй ряд кнопок
   const row2 = document.createElement("div");
   row2.classList.add("keyboard__row");
   section.appendChild(row2);
+
+  // кнопки второго ряда
+  for (let i = 0; i < keys2.length; i++) {
+    const key = document.createElement("div");
+    key.classList.add("keyboard__btn");
+    row2.appendChild(key);
+    for (let j = 0; j < keys2[i].length; j++) {
+        if (keys2[i].length === 2) {
+            const additionalSymbol = document.createElement("span");
+            additionalSymbol.classList.add("additional-symbol");
+            additionalSymbol.textContent = keys2[i][0];
+            key.appendChild(additionalSymbol);
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys2[i][j+1];
+            key.appendChild(mainSymbol);
+        }
+        else {
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys2[i][0];
+            key.appendChild(mainSymbol); 
+        }
+    }
+ }
  
   //третий ряд кнопок
   const row3 = document.createElement("div");
   row3.classList.add("keyboard__row");
   section.appendChild(row3);
+
+  // кнопки третьего ряда
+  for (let i = 0; i < keys3.length; i++) {
+    const key = document.createElement("div");
+    key.classList.add("keyboard__btn");
+    row3.appendChild(key);
+    for (let j = 0; j < keys3[i].length; j++) {
+        if (keys3[i].length === 2) {
+            const additionalSymbol = document.createElement("span");
+            additionalSymbol.classList.add("additional-symbol");
+            additionalSymbol.textContent = keys3[i][0];
+            key.appendChild(additionalSymbol);
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys3[i][j+1];
+            key.appendChild(mainSymbol);
+        }
+        else {
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys3[i][0];
+            key.appendChild(mainSymbol); 
+        }
+    }
+ }
    
   //четвертый ряд кнопок
   const row4 = document.createElement("div");
   row4.classList.add("keyboard__row");
   section.appendChild(row4);
    
+ // кнопки четвертого ряда
+ for (let i = 0; i < keys4.length; i++) {
+    const key = document.createElement("div");
+    key.classList.add("keyboard__btn");
+    row4.appendChild(key);
+    for (let j = 0; j < keys4[i].length; j++) {
+        if (keys4[i].length === 2) {
+            const additionalSymbol = document.createElement("span");
+            additionalSymbol.classList.add("additional-symbol");
+            additionalSymbol.textContent = keys4[i][0];
+            key.appendChild(additionalSymbol);
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys4[i][j+1];
+            key.appendChild(mainSymbol);
+        }
+        else {
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys4[i][0];
+            key.appendChild(mainSymbol); 
+        }
+    }
+ }
+
   //пятый ряд кнопок
   const row5 = document.createElement("div");
   row5.classList.add("keyboard__row");
   section.appendChild(row5);
+
+  // кнопки пятого ряда
+  for (let i = 0; i < keys5.length; i++) {
+    const key = document.createElement("div");
+    key.classList.add("keyboard__btn");
+    row5.appendChild(key);
+    for (let j = 0; j < keys5[i].length; j++) {
+        if (keys5[i].length === 2) {
+            const additionalSymbol = document.createElement("span");
+            additionalSymbol.classList.add("additional-symbol");
+            additionalSymbol.textContent = keys5[i][0];
+            key.appendChild(additionalSymbol);
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys5[i][j+1];
+            key.appendChild(mainSymbol);
+        }
+        else {
+            const mainSymbol = document.createElement("span");
+            mainSymbol.classList.add("main-symbol");
+            mainSymbol.textContent = keys5[i][0];
+            key.appendChild(mainSymbol); 
+        }
+    }
+ }
   
   
